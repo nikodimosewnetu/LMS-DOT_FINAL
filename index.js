10d9744b901e97c44ded749026e2fe6fc22a5c70
nikodimosewnetu <nikodimosewnetu@gmail.com>
2025-03-07 10:44:34 +0300

    Initial commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config({});
 connectDB();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // default middleware
 app.use(express.json());
